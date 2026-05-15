39f028d8a9485b8691e20db4677cce8de1d31404
playerkousas-rgb <playerkousas@gmail.com>
2026-05-16 05:38:23 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -159,12 +159,14 @@ export default function App() {
 
           {/* Chart Area */}
           <div className={isExpanded ? "h-[calc(70vh-42px)]" : "h-44"}>
-            <ElevationChart
-              profile={elevationProfile}
-              stats={stats}
-              waypoints={waypoints}
-              onHoverPoint={setHoveredPt}
-            />
+           <ElevationChart
+  profile={profile}
+  stats={stats}
+  waypoints={waypoints}
+  segments={segments}
+  naismithSettings={naismithSettings}
+  onHoverPoint={setHoverPoint}
+/>
           </div>
         </div>
 
